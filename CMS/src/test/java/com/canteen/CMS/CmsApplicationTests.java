cae436865fa7760c04d2139967008cbc31f17d5d
Supun Chalaka <supunchalaka@gmail.com>
2021-06-20 21:33:29 +0530

    project created
    dependencies - Spring Web WEB,
    Spring Data JPA,  MySQL Driver SQL, Thymeleaf TEMPLATE ENGINES, Spring Boot DevTools DEVELOPER TOOLS

---
+++ CMS/src/test/java/com/canteen/CMS/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canteen.CMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
