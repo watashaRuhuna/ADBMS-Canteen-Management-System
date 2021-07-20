6a3e8dc8e8bbff1f00ad362825a0513cbd160d2b
Supun Chalaka <supunchalaka@gmail.com>
2021-07-20 11:10:45 +0530

    changes

--- sqlqueries.sql
+++ sqlqueries.sql
@@ -14,7 +14,8 @@ beign
     declare foodqty int default 0;
     declare fooddate datetime;
     
-SELECT food_inventory.food_name into foodname, COUNT(order_table.food_id) AS QTY into foodqty, order_table.order_date AS DateF into fooddate FROM order_table,food_inventory WHERE food_inventory.food_id=order_table.food_id GROUP BY order_table.food_id, order_table.order_date;
+SELECT food_inventory.food_name into foodname, COUNT(order_table.food_id) AS QTY into foodqty, order_table.order_date
+AS DateF into fooddate FROM order_table,food_inventory WHERE food_inventory.food_id=order_table.food_id GROUP BY order_table.food_id, order_table.order_date;
 
 update order_count set count = foodqty, date = fooddate where food_name =foodname; 
 
@@ -22,7 +23,9 @@ end//
 
 
 
-CREATE VIEW  orderuserdetails AS SELECT order_table.order_id,order_table.food_type,order_table.is_cancel,order_table.is_done,order_table.order_date,food_inventory.food_name,user.user_name,user.user_id FROM order_table,food_inventory,user WHERE order_table.food_id=food_inventory.food_id AND order_table.user_id=user.user_id AND order_table.order_date = CURRENT_DATE()
+CREATE VIEW  orderuserdetails AS SELECT order_table.order_id,order_table.food_type,order_table.is_cancel,order_table.is_done,
+order_table.order_date,food_inventory.food_name,user.user_name,user.user_id FROM order_table,food_inventory,
+user WHERE order_table.food_id=food_inventory.food_id AND order_table.user_id=user.user_id AND order_table.order_date = CURRENT_DATE()
 
 
 delimiter //
@@ -35,7 +38,8 @@ begin
     declare foodtype varchar(30);
     declare foodqty int default 0;
     
-SELECT f.category_type into foodtype, COUNT(o.food_id) into foodqty, f.food_name into foodname FROM order_table o, food_inventory f WHERE o.order_date=curdate() and o.food_id=f.food_id GROUP by f.food_id;
+SELECT f.category_type into foodtype, COUNT(o.food_id) into foodqty, f.food_name into foodname FROM
+order_table o, food_inventory f WHERE o.order_date=curdate() and o.food_id=f.food_id GROUP by f.food_id;
 
 INSERT INTO order_count(count,date,food_name,foodtype)
 VALUE(foodqty,CURRENT_DATE(),foodname,foodtype);
